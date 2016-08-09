62397b3104c58f84dc83453fce8c78c5f2c70e9d
gourav <gourav96suri@gmail.com>
2016-08-09 11:27:52 +0530

    Implemented Speech feature

---
+++ app/src/main/java/thegenuinegourav/texttospeech/MainActivity.java
@@ -0,0 +1,64 @@
+package thegenuinegourav.texttospeech;
+
+import android.app.Activity;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Locale;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    TextToSpeech t1;
+    EditText ed1;
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ed1=(EditText)findViewById(R.id.editText);
+        b1=(Button)findViewById(R.id.button);
+
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toSpeak = ed1.getText().toString();
+                Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+
+    public void onPause(){
+        if(t1 !=null){
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
+    }
+
+}
